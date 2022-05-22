6b4638bb9dcccca7af09378bf8d7a5f3a1cdbee5
Asgor Ali <i.joni40@gmail.com>
2022-05-22 23:23:02 +0600

    Login Form Added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleLoginForm = e =>{
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+    }
+
     return (
-        <div className='px-24 mx-auto max-h-screen'>
-            <form>
-                <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
-            </form>
+        <div className='px-24 flex flex-col justify-center items-center h-screen'>
+            <form onSubmit={handleLoginForm} className="grid grid-cols-1 gap-5">
+                <input type="email" ref={emailRef} placeholder="Enter email" required />
+                <input type="password" ref={passwordRef} placeholder="Password" required />
+                <input type="submit" value='Login' className='btn btn-primary' />
+            </form>     
         </div>
     );
 };
