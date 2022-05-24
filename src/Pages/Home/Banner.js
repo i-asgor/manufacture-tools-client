1fa8b862539d96bc4a9c4fac463e72deb64c9199
Asgor Ali <i.joni40@gmail.com>
2022-05-25 04:43:26 +0600

    Manfacturing Name, Logo and banner fixed

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import tool from '../../images/tool.jpg';
+
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero h-screen bg-base-200 md:px-48">
             <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
+                <img src={tool} class="max-w-sm rounded-lg shadow-2xl" alt='tool' />
                 <div>
-                <h1 class="text-5xl font-bold">Box Office News!</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <h1 class="text-5xl font-bold">Explore the best Tools</h1>
+                <p class="py-6">We are the Quality Hand tools Manufacturer in Bangladesh and offers Engineering Excellence for the world’s most demanding industries. Our Tools range includes Industrial Tools, Precision Tools, Cutting Tools, Air Tools and Lubrication tools etc. As a Leader in Tools Manufacturing Industry, we always focus on Quality and customer satisfaction Which makes us different from other Tools Manufacturing Companies.</p>
                 <button class="btn btn-primary">Get Started</button>
                 </div>
             </div>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = ({children}) => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </label>
                 </div> 
-            <div class="flex-1">Navbar Title</div>
+            <div class="flex-1 text-4xl font-bold">AA Hand Tools</div>
             <div class="flex-none lg:hidden">
                 <label for="my-drawer-3" class="btn btn-square btn-ghost">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
